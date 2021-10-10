71a310a9c58574e63b2005fe02908a9ef41b34fb
Lucas Klauck <lucasklauck2@gmail.com>
2021-10-10 17:38:15 -0300

    Criação estrutura básica para listagem dos produtos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'projeto-shop';
+  readonly MOBILE = 800;
+
+  title = 'app-shop';
+  mobile: boolean = false;
+
+  ngOnInit() {
+    this.mobile = window.innerWidth < this.MOBILE;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.mobile = window.innerWidth < this.MOBILE;
+  }
 }
